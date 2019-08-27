2f5205f2418525c7c3140f9e90aaa1ca70fac7fc
kwon <kkdosk@naver.com>
2019-08-27 11:02:50 +0900

    load

--- hello.scm
+++ hello.scm
@@ -1,3 +1,14 @@
-
-(display "Hello World, this is my first Scheme program!!!")
+;(display "Hello World, this is my first Scheme program!!!")
 (* 4 4)
+
+(+ (* 3
+      (+ (* 2 4)
+         (+ 3 5)))
+  (+ (- 10 7)
+    6))
+
+; (define variable value)
+(display "\n")
+(define size 2)
+(display size)
+(display "\n")
