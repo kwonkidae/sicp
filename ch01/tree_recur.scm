09bfac798dff979d581c23e33ab10bbad8cdbf79
kwon <kkdosk@naver.com>
2019-08-30 13:37:17 +0900

    fast-expt

--- ch01/tree_recur.scm
+++ ch01/tree_recur.scm
@@ -37,3 +37,22 @@
   (if (not (> (abs angle) 0.1))
       angle
       (p (sine (/ angle 3.0)))))
+
+(define (expt b n)
+  (if (= n 0)
+      1
+      (* b (expt b (- n 1)))))
+
+(define (expt-i b n)
+  (expt-iter b n 1))
+(define (expt-iter b counter product)
+  (if (= counter 0)
+      product
+      (expt-iter b
+                 (- counter 1)
+                 (* b product))))
+
+(define (fast-expt b n)
+  (cond ((= n 0) 1)
+        ((even? n) (square (fast-expt b (/ n 2))))
+        (else (* b (fast-expt b (- n 1))))))
